9b15c933d81e920104b2806a135d93b5848036d7
Elena <lenulka.90@mail.ru>
2023-05-26 11:50:04 +0300

    Сдача домашки.

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,58 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
-        task3();
-        task4();
+
     }
 
     public static void task1() {
-        System.out.println(" ");
-        System.out.println("Задача 1");
-        //
-    }
+                System.out.println("Задачи 1-2");
+        // Объявите три массива по условиям.
+        // Распечатайте на отдельной строчке элементы каждого массива
+        // по порядку через запятую. В конце строки запятую ставить не надо.
+        int []    massiv1 = new int[] {1, 2, 3};
+        double [] massiv2 = {1.57, 7.654, 9.986};
+        int []    massiv3 = {17, 3, 24, 73, 16};
 
-    public static void task2() {
-        System.out.println(" ");
-        System.out.println("Задача 2");
-        //
-    }
+        System.out.println("Массив-1:");
+        System.out.println(Arrays.toString(massiv1));
+        System.out.println("Массив-2:");
+        System.out.println(Arrays.toString(massiv2));
+        System.out.println("Массив-3:");
+        System.out.println(Arrays.toString(massiv3));
 
-    public static void task3() {
-        System.out.println(" ");
+
         System.out.println("Задача 3");
-        //
-    }
+        // Распечатайте на отдельной строчке элементы каждого массива
+        // в обратном порядке через запятую. В конце строки запятую ставить не надо.
 
-    public static void task4() {
+        for (int i = massiv1.length - 1; i >= 0 ; i--) {
+            System.out.print(massiv1[i] + ", ");
+        }
+        System.out.println(" ");
+        for (int i = massiv2.length - 1; i >= 0 ; i--) {
+            System.out.print(massiv2[i] + ", ");
+        }
+        System.out.println(" ");
+        for (int i = massiv3.length - 1; i >= 0 ; i--) {
+            System.out.print(massiv3[i] + ", ");
+        }
+
         System.out.println(" ");
         System.out.println("Задача 4");
-        //
+        // Пройдитесь по первому целочисленному массиву и все нечетные числа в нем
+        // сделайте четными (нужно прибавить 1).
+        for (int i = 0; i < massiv1.length; i++) {
+            if (massiv1 [i] %2 != 0) {
+                massiv1[i]++;
+                            }
+        }
+
+        System.out.println(Arrays.toString(massiv1));
+
     }
+
 
 }
 
